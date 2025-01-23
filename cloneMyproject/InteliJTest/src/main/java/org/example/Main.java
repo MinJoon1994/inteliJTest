0a675956357392038156c6a14fc0d27bb8fc5e53
MinJoon1994 <rlaalswns199@gmail.com>
2025-01-23 10:40:19 +0900

    first Commit content

---
+++ cloneMyproject/InteliJTest/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        System.out.println("첫번째 Commit 내용");
+    }
+}
